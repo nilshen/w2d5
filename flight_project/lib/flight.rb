ce2f3a08d00cfb42a1f121adf16d4c95036f66fa
nilshen <linshen88@gmail.com>
2022-04-08 14:48:10 -0400

    flight done

---
+++ flight_project/lib/flight.rb
@@ -0,0 +1,36 @@
+require_relative "passenger"
+
+class Flight
+    attr_reader :passengers
+    def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = []
+    end
+
+    def full?
+        if @passengers.length == @capacity
+            return true
+        end
+    false
+    end
+
+    def board_passenger(passenger)
+        if !self.full? && passenger.has_flight?(@flight_number)
+            @passengers << passenger
+        end
+    end
+
+    def list_passengers
+        @passengers.map {|person|person.name}
+    end
+
+    def [](index)
+        @passengers[index]
+    end
+
+    def <<(passenger)
+        self.board_passenger(passenger)
+    end
+
+end
---
+++ flight_project/lib/passenger.rb
@@ -0,0 +1,29 @@
+
+class Passenger
+attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @flight_numbers = []
+
+    end
+
+    def has_flight?(flight_number)
+        if @flight_numbers.include?(flight_number.upcase)
+        return true
+        end
+        false
+    end
+
+
+    def add_flight(flight_number)
+        if !has_flight?(flight_number)
+            @flight_numbers << flight_number.upcase
+        else has_flight?(flight_number)
+        end
+    end
+
+
+
+
+end
